d7dbe1b416f26ce7032fbacffb6c45bb331a4ba4
kingofcrabs <rex.xie@qq.com>
2015-03-29 07:44:02 +0800

    temp

---
+++ ReadResult/ReadResult/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using HDLibrary.Wpf.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace ReadResult
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+
+        }
+
+        public XmlNode GetNode(XmlNodeList nodeList,string name)
+        {
+            foreach(XmlNode node in nodeList)
+            {
+                if (node.Name == name)
+                    return node;
+            }
+            throw new Exception("Cannot find");
+        }
+
+        void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            HotKeyHost hotKeyHost = new HotKeyHost((HwndSource)HwndSource.FromVisual(App.Current.MainWindow));
+            hotKeyHost.AddHotKey(new CustomHotKey("CreateNew", Key.N, ModifierKeys.Control, true));
+            hotKeyHost.AddHotKey(new CustomHotKey("StartAcq", Key.F5, ModifierKeys.None, true));
+
+             XmlDocument doc = new XmlDocument();      
+            try
+            {
+                doc.Load(@"F:\MultiUserReader\result\result.xml");    //加载Xml文件  
+                XmlElement rootElem = doc.DocumentElement;   //获取根节点  
+                XmlNode sectionNode = GetNode(rootElem.ChildNodes, "Section");
+                XmlNode dataNode = GetNode(sectionNode.ChildNodes, "Data");
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+
+
+    [Serializable]
+    public class CustomHotKey : HotKey
+    {
+        public CustomHotKey(string name, Key key, ModifierKeys modifiers, bool enabled)
+            : base(key, modifiers, enabled)
+        {
+            Name = name;
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    OnPropertyChanged(name);
+                }
+            }
+        }
+
+        protected override void OnHotKeyPress()
+        {
+            MessageBox.Show(string.Format("'{0}' has been pressed ({1})", Name, this));
+            base.OnHotKeyPress();
+        }
+
+
+        protected CustomHotKey(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+            : base(info, context)
+        {
+            Name = info.GetString("Name");
+        }
+
+        public override void GetObjectData(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+            info.AddValue("Name", Name);
+        }
+    }
+}
---
+++ ReadResult/ReadResult/SerializeHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ReadResult
+{
+    public static class SerializeHelper
+    {
+        /// <summary>
+        /// Serialize an instance of type T to a specified XML file
+        /// </summary>
+        /// <typeparam name="T">Type of object to be serialized</typeparam>
+        /// <param name="xmlFileName">XML file name</param>
+        /// <param name="objectGraph">object of type T</param>
+        public static void Serialize<T>(string xmlFileName, T objectGraph)
+        {
+            if (string.IsNullOrEmpty(xmlFileName))
+            {
+                throw new ArgumentException("文件不存在！", "xmlFileName");
+            }
+
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+                string sContent = "";
+                using (var ms = new MemoryStream())
+                {
+                    using (var xw = XmlWriter.Create(ms,
+                        new XmlWriterSettings()
+                        {
+                            Encoding = new UTF8Encoding(false),
+                            Indent = true,
+                            NewLineOnAttributes = true,
+                        }))
+                    {
+                        serializer.Serialize(xw, objectGraph);
+                        sContent = Encoding.UTF8.GetString(ms.ToArray());
+                    }
+                }
+                File.WriteAllText(xmlFileName, sContent);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Failed to serialize the object : " + ex.Message);
+            }
+
+        }
+
+        /// <summary>
+        /// Deserialize an XML file to an instance of type T
+        /// </summary>
+        /// <typeparam name="T">Type of object to be deserialized</typeparam>
+        /// <param name="xmlFileName">XML file name</param>
+        /// <returns>object of type T</returns>
+        public static T Deserialize<T>(string xmlFileName)
+        {
+
+            if (!File.Exists(xmlFileName))
+            {
+                string errorMessage = string.Format("文件不存在！", xmlFileName);
+                throw new ArgumentException(errorMessage, "xmlFileName");
+            }
+
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+
+            using (Stream fs = new FileStream(xmlFileName, FileMode.Open, FileAccess.Read, FileShare.None))
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.DtdProcessing = DtdProcessing.Parse;
+
+                using (XmlReader reader = XmlReader.Create(fs, settings))
+                {
+                    return (T)serializer.Deserialize(reader);
+                }
+            }
+        }
+
+
+    }
+}
